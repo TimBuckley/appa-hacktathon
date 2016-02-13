4db8af7e6cd193495dfe6ea6bdc77c73023b855b
fpcyan <fconn@mdsol.com>
2016-02-13 16:46:33 -0500

    Create graduates table

--- happy_camper/db/migrate/20160213214148_create_graduates.rb
+++ happy_camper/db/migrate/20160213214148_create_graduates.rb
@@ -1,6 +1,14 @@
 class CreateGraduates < ActiveRecord::Migration
   def change
     create_table :graduates do |t|
+      t.string :graduate_uuid, null: false, index: true, unique: true
+      t.string :first_name
+      t.string :last_name
+      t.string :picture_url
+      t.string :month_attended
+      t.integer :year_attended
+      t.string :bootcamp_name, default: 'App Academy'
+      t.string :bootcamp_location, default: 'San Francisco'
 
       t.timestamps null: false
     end
