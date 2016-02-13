9dc02cb75c49d96326e72411db1a40e5a57be11a
fpcyan <fconn@mdsol.com>
2016-02-13 17:38:05 -0500

    add associations to graduate

--- happy_camper/app/models/graduate.rb
+++ happy_camper/app/models/graduate.rb
@@ -1,2 +1,19 @@
 class Graduate < ActiveRecord::Base
+  validates_presence_of :uuid, :full_name
+  validates_uniqueness_of :uuid
+  after_initialize :ensure_uuid
+
+  has_one :github, class_name: "GithubDatum"
+  has_one :capstone
+  has_many :work_histories
+
+  private
+
+  def ensure_uuid
+    self.uuid || generate_uuid.save
+  end
+
+  def generate_uuid
+    SecureRandom::urlsafe_base64(12)
+  end
 end
