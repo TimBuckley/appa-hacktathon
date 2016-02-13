9807745b378b0511548ce3f93dc5ddcf7b7c7eeb
fpcyan <fconn@mdsol.com>
2016-02-13 16:51:38 -0500

    change graduates model and add github data model

---
+++ happy_camper/db/migrate/20160213214844_create_github_data.rb
@@ -0,0 +1,16 @@
+class CreateGithubData < ActiveRecord::Migration
+  def change
+    create_table :github_data do |t|
+      t.integer :graduate_id, null: false, index: true
+      t.string :url
+      t.string :username
+      t.integer :number_of_repos
+      t.string :personal_website_url
+
+      t.timestamps null: false
+    end
+
+    remove_column :graduates, :last_name
+    change_column :graduates, :first_name, :full_name
+  end
+end
