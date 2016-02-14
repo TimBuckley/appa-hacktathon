b92f6325fb5b6595d2389a7075b3ecebf31e6bf2
fpcyan <fconn@mdsol.com>
2016-02-14 01:16:40 -0500

    update work history and readme is now for justin

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# appa-hacktathon
-App Academy NYC Hackathon project
+## Create work histories and companies
 
+suggested routes
+```
+graduate = Graduate.find_by_uuid("example-uuid")
+graduate.work_histories.create(
+  title: "Software Engineer",
+  date_hired: DateTime.new(2015, 12, 28),
+  first_job: true,
+  current: true,
+  company_name: "Apple",
+  company_website: "https://www.apple.com"
+)
+```
--- happy_camper/app/models/work_history.rb
+++ happy_camper/app/models/work_history.rb
@@ -1,30 +1,25 @@
-# WorkHistory.create(graduate_id: graduate_id, company_name: "google", company_website: "google.com", location: "NYC", current: true, first_job: true)
+# WorkHistory.create(graduate_id: graduate_id, company_name: "google", company_website: "google.com", :title, ...)
+# OR
+# WorkHistory.create(:graduate_id, :company_id, :title, ...)
 class WorkHistory < ActiveRecord::Base
   validates_uniqueness_of :current, scope: :graduate_id
 
   belongs_to :company
   belongs_to :graduate
 
-  attr_reader :company_name, :company_website
+  attr_accessor :company_name, :company_website
 
   after_initialize :associate_company
 
-  def company_name=(company_name)
-    company_name.titleize
-  end
-
-  def company_website=(company_website)
-    company_website.titleize
-  end
   private
 
   def associate_company
     if company_name || company_website
-      self.company = Company.find_by_name(company_name) || Company.find_by_website(company_website) || make_company
+      self.company = Company.find_by_name(company_name.titleize) || Company.find_by_website(company_website) || make_company
     end
   end
 
   def make_company
-    Company.create(name: company_name, website: company_website)
+    Company.create(name: company_name.titleize, website: company_website)
   end
 end
